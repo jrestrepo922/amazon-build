5d88ebfb1bca64b8727e4e0711bfbb514d8ee4db
jrestrepo922 <tinto922@gmail.com>
2021-08-04 08:46:58 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLiTKgwR-oAZn5hRlBH3iZDq_CsUjCqFo",
+    authDomain: "build-ae5ff.firebaseapp.com",
+    projectId: "build-ae5ff",
+    storageBucket: "build-ae5ff.appspot.com",
+    messagingSenderId: "833324462241",
+    appId: "1:833324462241:web:8959abd58e75f1237380e0"
+  };
